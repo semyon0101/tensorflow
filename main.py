fdce8c461509bb7cdadfed9f1a346809b54d0773
Semyon <79764144+semyon0101@users.noreply.github.com>
2024-10-25 19:40:40 +0300

    new

--- main.py
+++ main.py
@@ -39,8 +39,10 @@ result = tf.multiply(x1, x2)
 with tf.Session() as sess:
   output = sess.run(result)
   print(output)"""
+
 """import numpy as np
 import math
+np.random.seed(0)
 
 def sigmoid(x):
     return 1/(1+math.e**(-x))
@@ -60,67 +62,48 @@ learningRate = 0.3
 sigFunc = np.vectorize(sigmoid)
 sigDXFunc = np.vectorize(sigmoidDX)
 
-w1 = np.random.rand(3, 2)
-w2 = np.random.rand(2, 1)
+w1 = np.random.rand(4, 3)
+w2 = np.random.rand(4, 2)
+w3 = np.random.rand(3, 1)
 
 
 for _ in range(1000):
-    print(_)
+    #print(_)
     for i in range(dataI.shape[0]):
         n1 = np.matrix(dataI[i]).T
-        n2 = sigFunc(np.dot(w1.T,n1))
-        n3 = sigFunc(np.dot(w2.T,n2))
-        #print(dataI[i], dataO[i], n3[0,0]) 
+        n1 = np.r_[n1, np.ones((1, 1))]
+        n2 = sigFunc(np.dot(w1.T, n1))
+        n2 = np.r_[n2, np.ones((1, 1))]
+        n3 = sigFunc(np.dot(w2.T, n2))
+        n3 = np.r_[n3, np.ones((1, 1))]
+        n4 = sigFunc(np.dot(w3.T, n3))
 
-        d3 = sigDXFunc(n3-dataO[i]).T
-        w2 = w2 - np.dot(n2,  d3) * learningRate
+        d4 = sigDXFunc(n4-dataO[i]).T
+        w3 = w3 - np.dot(n3, d4) * learningRate
 
-        d2 = sigDXFunc(np.dot(w2, d3)).T
+        d3 = (np.dot(d4, np.delete(w3.T, w3.T.shape[0]-1, 1)))
+        w2 = w2 - np.dot(n2, d3) * learningRate
+
+        d2 = (np.dot(d3, np.delete(w2.T, w2.T.shape[0]-1, 1)))
         w1 = w1 - np.dot(n1, d2) * learningRate
         
 
 for i in range(dataI.shape[0]):
     n1 = np.matrix(dataI[i]).T
-    n2 = sigFunc(np.dot(w1.T,n1))
-    n3 = sigFunc(np.dot(w2.T,n2))
-    print(dataI[i], dataO[i], n3[0,0])""" 
+    n1 = np.r_[n1, np.ones((1, 1))]
+    n2 = sigFunc(np.dot(w1.T, n1))
+    n2 = np.r_[n2, np.ones((1, 1))]
+    n3 = sigFunc(np.dot(w2.T, n2))
+    n3 = np.r_[n3, np.ones((1, 1))]
+    n4 = sigFunc(np.dot(w3.T, n3))
+    print(dataI[i], dataO[i], n4[0,0])
+"""
+import matplotlib.pyplot as plt
 import numpy as np
-import math
+import sklearn
+import sklearn.datasets
 
-def sigmoid(x):
-    return 1/(1+math.e**(-x))
-
-def sigmoidDX(sig):
-    return sig*(1-sig)
-
-def answer(a):
-    return int((a[0] + a[1] - a[2])>0)
-
-dataI = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
-dataO = np.array([answer(a) for a in dataI])
-print(dataI)
-print(dataO)
-learningRate = 0.3
-
-sigFunc = np.vectorize(sigmoid)
-sigDXFunc = np.vectorize(sigmoidDX)
-
-w1 = np.random.rand(4, 1)
-
-
-for _ in range(1000):
-    print(_)
-    for i in range(dataI.shape[0]):
-        n1 = np.matrix(dataI[i]).T
-        n1 = # нужен сдвиг  bias !!!!
-        n2 = sigFunc(np.dot(w1.T,n1))
-
-        d2 = sigDXFunc(n2-dataO[i]).T
-        w1 = w1 - np.dot(n1,  d2) * learningRate
-        
-
-for i in range(dataI.shape[0]):
-    n1 = np.matrix(dataI[i]).T
-    n2 = sigFunc(np.dot(w1.T,n1))
-    print(dataI[i], dataO[i], n2[0,0])
-
+np.random.seed(0)
+X, y = sklearn.datasets.make_moons(200, noise=0.20)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+plt.show()
