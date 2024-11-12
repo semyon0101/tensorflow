8c41ee8ec56e342a0bf2e502d5a5b7a75451c43e
Semyon <79764144+semyon0101@users.noreply.github.com>
2024-11-12 19:59:31 +0300

    new

--- main.py
+++ main.py
@@ -40,9 +40,15 @@ with tf.Session() as sess:
   output = sess.run(result)
   print(output)"""
 
-"""import numpy as np
+import numpy as np
 import math
 np.random.seed(0)
+
+def answer1(a):
+    return int((a[0, 0] + a[0, 1] - a[0, 2])>0)
+
+def answer2(a):
+    return int((-a[0, 0] - a[0, 1] + a[0, 2])>=0)
 
 def sigmoid(x):
     return 1/(1+math.e**(-x))
@@ -50,60 +56,79 @@ def sigmoid(x):
 def sigmoidDX(sig):
     return sig*(1-sig)
 
-def answer(a):
-    return int((a[0] + a[1] - a[2])>0)
+def calculate_loss():
+    loss = np.sum(np.power((predict(dataI)-dataO), 2))
+    return loss
 
-dataI = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
-dataO = np.array([answer(a) for a in dataI])
+def predict(x):
+    a0 = np.append(np.matrix(x), np.ones((x.shape[0], 1)), 1)
+
+    z1 = a0.dot(w1)
+    a1 = np.append(sigFunc(z1), np.ones((x.shape[0], 1)), 1)
+
+    z2 = a1.dot(w2)
+    a2 = np.append(sigFunc(z2), np.ones((x.shape[0], 1)), 1)
+
+    z3 = a2.dot(w3)
+    a3 = np.append(sigFunc(z3), np.ones((x.shape[0], 1)), 1)
+
+    z4 = a3.dot(w4)
+    a4 = np.append(sigFunc(z4), np.ones((x.shape[0], 1)), 1)
+    
+    z5 = a4.dot(w5)
+    a5 = sigFunc(z5)
+
+    return a5
+
+dataI = np.matrix([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
+dataO = np.matrix([[answer1(a), answer2(a)] for a in dataI])
 print(dataI)
 print(dataO)
-learningRate = 0.3
+learningRate = 0.03
 
 sigFunc = np.vectorize(sigmoid)
 sigDXFunc = np.vectorize(sigmoidDX)
 
-w1 = np.random.rand(4, 3)
-w2 = np.random.rand(4, 2)
-w3 = np.random.rand(3, 1)
+w1 = np.random.rand(3 + 1, 3)
+w2 = np.random.rand(3 + 1, 3)
+w3 = np.random.rand(3 + 1, 3)
+w4 = np.random.rand(3 + 1, 3)
+w5 = np.random.rand(3 + 1, 2)
 
+for _ in range(100000):
+    a0 = np.append(np.matrix(dataI), np.ones((dataI.shape[0], 1)), 1)
 
-for _ in range(1000):
-    #print(_)
-    for i in range(dataI.shape[0]):
-        n1 = np.matrix(dataI[i]).T
-        n1 = np.r_[n1, np.ones((1, 1))]
-        n2 = sigFunc(np.dot(w1.T, n1))
-        n2 = np.r_[n2, np.ones((1, 1))]
-        n3 = sigFunc(np.dot(w2.T, n2))
-        n3 = np.r_[n3, np.ones((1, 1))]
-        n4 = sigFunc(np.dot(w3.T, n3))
+    z1 = a0.dot(w1)
+    a1 = np.append(sigFunc(z1), np.ones((dataI.shape[0], 1)), 1)
 
-        d4 = sigDXFunc(n4-dataO[i]).T
-        w3 = w3 - np.dot(n3, d4) * learningRate
+    z2 = a1.dot(w2)
+    a2 = np.append(sigFunc(z2), np.ones((dataI.shape[0], 1)), 1)
 
-        d3 = (np.dot(d4, np.delete(w3.T, w3.T.shape[0]-1, 1)))
-        w2 = w2 - np.dot(n2, d3) * learningRate
+    z3 = a2.dot(w3)
+    a3 = np.append(sigFunc(z3), np.ones((dataI.shape[0], 1)), 1)
 
-        d2 = (np.dot(d3, np.delete(w2.T, w2.T.shape[0]-1, 1)))
-        w1 = w1 - np.dot(n1, d2) * learningRate
+    z4 = a3.dot(w4)
+    a4 = np.append(sigFunc(z4), np.ones((dataI.shape[0], 1)), 1)
+    
+    z5 = a4.dot(w5)
+    a5 = sigFunc(z5)
+
+    deltaZ5 = sigDXFunc(a5-dataO)
+    deltaZ4 = np.multiply(deltaZ5.dot(np.delete(w5.T, w5.T.shape[1]-1, 1)), np.delete(sigDXFunc(a4), a4.shape[1]-1, 1))
+    deltaZ3 = np.multiply(deltaZ4.dot(np.delete(w4.T, w4.T.shape[1]-1, 1)), np.delete(sigDXFunc(a3), a3.shape[1]-1, 1))
+    deltaZ2 = np.multiply(deltaZ3.dot(np.delete(w3.T, w3.T.shape[1]-1, 1)), np.delete(sigDXFunc(a2), a2.shape[1]-1, 1))
+    deltaZ1 = np.multiply(deltaZ2.dot(np.delete(w2.T, w2.T.shape[1]-1, 1)), np.delete(sigDXFunc(a1), a1.shape[1]-1, 1))
+    
+    w5 -= a4.T.dot(deltaZ5) * learningRate
+    w4 -= a3.T.dot(deltaZ4) * learningRate  
+    w3 -= a2.T.dot(deltaZ3) * learningRate
+    w2 -= a1.T.dot(deltaZ2) * learningRate
+    w1 -= a0.T.dot(deltaZ1) * learningRate
+
+    if _ % 1000 == 0:
+        print (f"Loss after iteration {_}: { calculate_loss()}")
         
 
 for i in range(dataI.shape[0]):
-    n1 = np.matrix(dataI[i]).T
-    n1 = np.r_[n1, np.ones((1, 1))]
-    n2 = sigFunc(np.dot(w1.T, n1))
-    n2 = np.r_[n2, np.ones((1, 1))]
-    n3 = sigFunc(np.dot(w2.T, n2))
-    n3 = np.r_[n3, np.ones((1, 1))]
-    n4 = sigFunc(np.dot(w3.T, n3))
-    print(dataI[i], dataO[i], n4[0,0])
-"""
-import matplotlib.pyplot as plt
-import numpy as np
-import sklearn
-import sklearn.datasets
-
-np.random.seed(0)
-X, y = sklearn.datasets.make_moons(200, noise=0.20)
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
-plt.show()
+    print(dataI[i], dataO[i], predict(dataI[i]))
+print(calculate_loss())
