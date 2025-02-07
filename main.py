1faa36f56e04a0c74d6b24553f2d831f0f7565f0
Semyon <79764144+semyon0101@users.noreply.github.com>
2025-02-07 19:51:14 +0300

    0.2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
+import requests
+from bs4 import BeautifulSoup
+req = requests.get("https://mubi.com/en/films?sort=popularity_quality_score")
+src = req.text
+soup = BeautifulSoup(src, 'lxml')
+title = soup.title.string
+print(title)
