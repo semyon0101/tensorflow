2ae7449d48cf8493f0cd2e059ad71db0c64b7c4b
semeon <79764144+semyon0101@users.noreply.github.com>
2025-02-10 15:23:24 +0300

    0.3

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-import requests
+from concurrent.futures import as_completed
+from requests_futures.sessions import FuturesSession
 from bs4 import BeautifulSoup
-req = requests.get("https://mubi.com/en/films?sort=popularity_quality_score")
-src = req.text
-soup = BeautifulSoup(src, 'lxml')
-title = soup.title.string
-print(title)
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def openf(req):
+    
+    soup = BeautifulSoup(req.text, 'lxml')
+    img =  soup.find_all('img')[0]
+
+    imageLink = img.get('src')
+    title = img.get('alt')
+    if len(soup.find_all('p'))==1:
+        description = soup.find_all('p')[0].string 
+    else:
+        description = soup.find_all('p')[1].string 
+    creationYear = soup.title.string.split(title)[1][2:6]
+    return [req.url, imageLink, title, description, creationYear]
+
+
+with open('profiles1.csv', 'w', newline='', encoding="utf-8") as file:
+    session = FuturesSession()
+
+    writer = csv.writer(file)
+    field = ["ref", "imageLink", "title", "description", "creationYear"]
+    writer.writerow(field)
+
+    driver = webdriver.Chrome()
+    i = 1
+    while i!=416:
+        print('page: '+str(i))
+        driver.get('https://mubi.com/en/films?sort=popularity_quality_score&page='+str(i))
+        ar = []
+        while True:
+            time.sleep(1)
+            ar = driver.find_elements(By.TAG_NAME, 'a')
+            if len(ar)==55:
+                break
+        l = []
+        for a in ar:
+            if a.get_attribute('data-testid')=="film-tile-link":
+
+                l.append(session.get(a.get_attribute('href')))
+        
+        for future in as_completed(l):
+            writer.writerow(openf(future.result()))
+        
+        time.sleep(1)
+        i+=1
+        
