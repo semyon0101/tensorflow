a63615be20c774f5cbbad6f1fc32f612a6237635
Semyon <79764144+semyon0101@users.noreply.github.com>
2025-02-11 19:52:37 +0300

    0.6

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ def beautifulString(str):
 
 def openf(req):
     soup = BeautifulSoup(req.text, 'lxml')
-    name = soup.find_all('h1')[0].string[6:]
-    image = soup.find('div', class_ = 'poster movie').find('img').get("src")
+    name = beautifulString(soup.find_all('h1')[0].string[6:])
+    image = beautifulString(soup.find('div', class_ = 'poster movie').find('img').get("src"))
     description = beautifulString(soup.find("div", class_='description').text)
-    creationYear = soup.find('ul', class_ = 'attributes').find_all('li')[1].text
+    for year in soup.find('ul', class_ = 'attributes').find_all('li'):
+        if len(year.text)==4:
+            creationYear = beautifulString(year.text)
+    
     return [req.url, name, image, description, creationYear]
 
 api_url = 'https://www.yidio.com/redesign/json/browse_results.php'
-params = {"type": "movie", "index": "0", "limit": "10000"}
+params = {"type": "movie", "index": "0", "limit": "1000"}
 
 with open('profiles1.csv', 'w', newline='', encoding="utf-8") as file:
     session = FuturesSession()
@@ -30,7 +33,7 @@ with open('profiles1.csv', 'w', newline='', encoding="utf-8") as file:
     writer = csv.writer(file)
     field = ["url", "name", "image", "description", "creationYear"]
     writer.writerow(field)
-    for params['index'] in range(1,40000,10000): 
+    for params['index'] in range(1,40000,1000): 
         print(params["index"])
         data = requests.get(api_url, params=params).json()
         l = []
