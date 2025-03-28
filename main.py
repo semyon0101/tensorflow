dd00d660b88c83bf82481657711ae353d83158cd
Semyon <79764144+semyon0101@users.noreply.github.com>
2025-03-28 18:11:21 +0300

    0.1

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests 
-from bs4 import BeautifulSoup
-
-url = "https://www.film.ru/movies/memuary-ulitki"
-req = requests.request('get',url)
-soup = BeautifulSoup(req.text, 'html.parser')
-
-f = open('test.txt', 'w', encoding="utf-8")
-f.write(req.text)
